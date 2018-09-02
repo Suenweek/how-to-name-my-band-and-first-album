032c73b80b66e57da1997653a6d5cb47adabe0eb
Roman Novatorov <roman.novatorov@gmail.com>
2018-09-03 01:27:03 +0300

    - Clean-up

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import requests
 
 
 RANDOM_ARTICLE_URL = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
-RE = re.compile(r'([\w\d\-\s]+[\w\d]+)')
+RE_STRIP_HEADING = re.compile(r'([\w\d\-\s]+[\w\d]+)')
 
 
 def get_random_article_heading():
@@ -14,16 +14,18 @@ def get_random_article_heading():
     return heading.text
 
 
-def strip(heading):
-    return RE.match(heading).groups()[0]
+def strip_heading(heading):
+    return RE_STRIP_HEADING.match(heading).groups()[0]
 
 
 def get_random_band_name():
-    return strip(get_random_article_heading())
+    heading = get_random_article_heading()
+    return strip_heading(heading)
 
 
 def get_random_first_album_name():
-    return strip(get_random_article_heading())
+    heading = get_random_article_heading()
+    return strip_heading(heading)
 
 
 def main():
